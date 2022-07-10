e5ecb7f21cd363103d0d13559276db27bc90ecf5
yubarajshrestha <companion.krish@outlook.com>
2022-07-10 05:22:40 +0000

    Deploying to gh-pages from @ py-package/official-website@be58775b3fb43c7f3dbf2b1611faa77152c848c6 🚀

--- _next/static/4LaFmF751c6s9KZHjhack/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-e4a7398816f78236.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
