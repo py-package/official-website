86cd384be86505b8b0e18aa7b55586341b4cb29a
Yubaraj Shrestha <companion.krish@outlook.com>
2022-07-24 21:06:04 +0545

    Added Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout';
+import Script from 'next/script';
 
 function PyPackageApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-TV45ZD32T8" />
+      <Script strategy="lazyOnload" id='scripts'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-TV45ZD32T8', {
+                page_path: window.location.pathname,
+              });
+          `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
