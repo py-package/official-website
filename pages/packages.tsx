f0b01e60223ae34897909b0f1799c155ac5bb947
Yubaraj Shrestha <companion.krish@outlook.com>
2022-08-12 18:03:18 +0545

    added script

--- pages/packages.tsx
+++ pages/packages.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable react/no-children-prop */
 import matter from 'gray-matter'
 import type { NextPage } from 'next'
+import Script from 'next/script'
 import Head from 'next/head'
 import Page from '../components/page'
 // import md from 'markdown-it';
@@ -56,6 +57,7 @@ const PackagesPage: NextPage = () => {
         <title>PyPackage 0.0.1 - Packages</title>
       </Head>
       <main className='md:px-32 px-16 pt-4'>
+        <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0660597578690897" crossOrigin="anonymous"></Script>
         <h1 className='text-3xl font-bold text-center mt-12'>Packages</h1>
         <p className="text-center mb-8">Followings are some packages published and are ready to use. There are more packages coming on the way. Enjoy using the framework <a href="https://docs.masoniteproject.com/" rel='noreferrer' target="_blank" className="text-red-600 font-semibold">Masonite<sup>4.0</sup></a></p>
         <section className='grid gap-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 w-full mt-4'>
